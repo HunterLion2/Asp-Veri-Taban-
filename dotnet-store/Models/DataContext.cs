a9d20f7ba1d93a89b1efbf2052b001d25b0706a4
Çınar Özgürgil <cozgurgil@gmail.com>
2025-04-06 18:03:26 +0300

    Data-Seeding

--- dotnet-store/Models/DataContext.cs
+++ dotnet-store/Models/DataContext.cs
@@ -17,5 +17,42 @@ public class DataContext : DbContext {
     // Urun adında bir model sınıfınız olduğunu varsayar. Bu model, Urunler tablosunun yapısını tanımlar.
     // Uygulama içinde Urunler üzerinden sorgular yaparak Urun tablosundaki verilere erişebilirsiniz.
 
+    // OnModelCreating, Entity Framework Core'da veritabanı modeli oluşturulurken çağrılan bir metottur.
+    // Bu metot, veritabanı tablolarının ve ilişkilerinin nasıl oluşturulacağını özelleştirmek için kullanılır.
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Aslında buraya çağırılan bilgiler direkt olarak veritabanı açıldığında gelicek olan bilgilerdir.
+        // Buraya yazılan bilgileri kaydetmek için "Mignations" oluşturulur yani aslında bu şekilde kalıcı bilgi oluşturulduktan sonra Migrations değeri çağırılır.
+
+        modelBuilder.Entity<Urun>().HasData(
+            new List<Urun> {
+                new Urun() { Id = 1, UrunAdi= "Apple Watch 7", Fiyat= 10000, Aktif= true },
+                new Urun() { Id = 2, UrunAdi= "Apple Watch 8", Fiyat= 20000, Aktif= false },
+                new Urun() { Id = 3, UrunAdi= "Apple Watch 9", Fiyat= 30000, Aktif= true },
+                new Urun() { Id = 4, UrunAdi= "Apple Watch 10", Fiyat= 40000, Aktif= true },
+                new Urun() { Id = 5, UrunAdi= "Apple Watch 11", Fiyat= 50000, Aktif= false },
+                new Urun() { Id = 6, UrunAdi= "Apple Watch 12", Fiyat= 60000, Aktif= true },
+            }
+        );
+    }
+
+    // ---------------------------------------------------------------
+
+    // 3. modelBuilder.Entity<Urun>()
+
+    // modelBuilder.Entity<Urun>() ifadesi, Urun modeline ait yapılandırmaları tanımlamak için kullanılır.
+    // Bu, Urun modelinin veritabanındaki bir tabloya karşılık geldiğini belirtir.
+    
+    // 4. HasData()
+
+    // HasData() metodu, veritabanına başlangıç verileri (seed data) eklemek için kullanılır.
+    // Bu veriler, veritabanı oluşturulduğunda otomatik olarak tabloya eklenir.
+    // Örneğin, burada Urun tablosuna 6 adet ürün eklenmiştir.
+
+    // ----------------------------------------------------------------
+
 }
 
