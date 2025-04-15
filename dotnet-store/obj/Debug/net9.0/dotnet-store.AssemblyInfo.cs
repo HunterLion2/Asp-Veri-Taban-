67f31e605a331b00ae5c46bed442ea17448f5897
Çınar Özgürgil <cozgurgil@gmail.com>
2025-04-15 11:35:30 +0300

    Navbar-ViewComponent-Finish

--- dotnet-store/obj/Debug/net9.0/dotnet-store.AssemblyInfo.cs
+++ dotnet-store/obj/Debug/net9.0/dotnet-store.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba0ef941add7ef3f503bf0e682d77bb831e667ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19da9f7ea33011fb8d488bf8e4941686052d69af")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
